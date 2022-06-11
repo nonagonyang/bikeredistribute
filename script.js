edd51775fd8ab0f662c6cbeb80c21b7266fc1844
YANG LIU <>
2022-06-11 21:12:49 +0100

    add buttons

--- script.js
+++ script.js
@@ -10,9 +10,49 @@
 // magnification with which the map will start
 const zoom = 18;
 
-// co-ordinates
-const lat = 51.5182516; 
-const lng = -0.0890625;
+// co-ordinates => user's location 
+let lat = 51.5182516; 
+let lng = -0.0890625;
+
+// let lat
+// let lng 
+
+
+function geoFindUser() {
+
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    lat  = position.coords.latitude;
+    lng = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${lat}/${lng}`;
+    mapLink.textContent = `Latitude: ${lat} °, Longitude: ${lng} °`;
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+}
+
+document.querySelector('#find-me').addEventListener('click', geoFindUser);
+
+
+
+
 
 async function main(){
   const bikeCoords = await getBikeStops()
@@ -28,9 +68,13 @@ async function main(){
 
 bikeCoords.map(({lat,lon, numBikes, numDocks, numEmptyDocks, name}) => {
 
-  marker = new L.marker([lat, lon]).bindPopup(`${name} (${numEmptyDocks} docks empty) with ${numBikes} bikes available`).addTo(map);
+  marker = new L.marker([lat, lon]).bindPopup(`${name} (${numEmptyDocks} docks empty) with ${numBikes} bikes available the emptyness percentage: ${numEmptyDocks}/${numDocks}`).addTo(map);
 })
+
 }
+
+
+
 
 
 // fetch bikestops, bike numbers, dock numbers
@@ -47,8 +91,6 @@ async function getBikeStops(){
   }))
   return coords
 }
-// getBikeStops().then(nBikes=> console.log(nBikes))
-
 
 
 main()
